5d0140bbb2ca0bdf4a39fc50865d5ac0f28063ff
Ptitlu42 <lucas.beyer@gmx.fr>
2023-12-09 21:58:53 +0100

    Create Plants table and seeder 🌱

---
+++ database/seeders/PlantsTableSeeder.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PlantsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        $plants = [
+            [
+                'owner_id' => 1,
+                'specie_name' => 'Rose',
+                'location' => 'Jardin',
+                'url_photo' => 'https://ibb.co/qrfthKs',
+                'status' => 'En bonne santé',
+                'description' => 'Belle rose rouge épanouie.'
+            ],
+            [
+                'owner_id' => 1,
+                'specie_name' => 'Tulipe',
+                'location' => 'Balcon',
+                'url_photo' => 'https://ibb.co/qrfthKs',
+                'status' => 'Besoin d\'eau',
+                'description' => 'Tulipes jaunes nécessitant un arrosage régulier.'
+            ],
+            [
+                'owner_id' => 2,
+                'specie_name' => 'Chêne',
+                'location' => 'Cour arrière',
+                'url_photo' => 'https://ibb.co/qrfthKs',
+                'status' => 'Robuste',
+                'description' => 'Grand chêne avec une large canopée.'
+            ],
+            [
+                'owner_id' => 2,
+                'specie_name' => 'Lavande',
+                'location' => 'Jardin avant',
+                'url_photo' => 'https://ibb.co/qrfthKs',
+                'status' => 'Florissante',
+                'description' => 'Lavande parfumée attirant les abeilles et les papillons.'
+            ],
+            [
+                'owner_id' => 3,
+                'specie_name' => 'Cactus',
+                'location' => 'Fenêtre intérieure',
+                'url_photo' => 'https://ibb.co/qrfthKs',
+                'status' => 'Nécessite peu de soins',
+                'description' => 'Un cactus résistant nécessitant peu d\'arrosage.'
+            ],
+            [
+                'owner_id' => 3,
+                'specie_name' => 'Orchidée',
+                'location' => 'Serre',
+                'url_photo' => 'https://ibb.co/qrfthKs',
+                'status' => 'Délicate',
+                'description' => 'Orchidée exotique avec de magnifiques fleurs.'
+            ],
+            [
+                'owner_id' => 4,
+                'specie_name' => 'Bambou',
+                'location' => 'Bord de l\'étang',
+                'url_photo' => 'https://ibb.co/qrfthKs',
+                'status' => 'Croissance rapide',
+                'description' => 'Bambou vert poussant rapidement près de l\'eau.'
+            ],
+            [
+                'owner_id' => 4,
+                'specie_name' => 'Érable japonais',
+                'location' => 'Jardin japonais',
+                'url_photo' => 'https://ibb.co/qrfthKs',
+                'status' => 'Coloré',
+                'description' => 'Petit érable avec des feuilles rouges et oranges éclatantes.'
+            ]
+
+        ];
+
+        foreach ($plants as $plant) {
+            DB::table('plants')->insert($plant);
+        }
+    }
+}
