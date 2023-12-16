e2a340ec5a7793e989924e56cc9020e762e1d067
Ptitlu42 <lucas.beyer@gmx.fr>
2023-12-16 13:34:06 +0100

    Add log into discord 🔥

--- config/app.php
+++ config/app.php
@@ -168,6 +168,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
+        MarvinLabs\DiscordLogger\ServiceProvider::class,
     ])->toArray(),
 
     /*
--- config/logging.php
+++ config/logging.php
@@ -53,8 +53,8 @@ return [
 
     'channels' => [
         'stack' => [
-            'driver' => 'stack',
-            'channels' => ['single'],
+            'driver'   => 'stack',
+            'channels' => ['single', 'discord'],
             'ignore_exceptions' => false,
         ],
 
@@ -125,6 +125,14 @@ return [
 
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
+        ],
+
+        'discord' => [
+            'driver' => 'custom',
+            'via'    => MarvinLabs\DiscordLogger\Logger::class,
+            'level'  => 'debug',
+            'url'    => env('LOG_DISCORD_WEBHOOK_URL'),
+            'ignore_exceptions' => env('LOG_DISCORD_IGNORE_EXCEPTIONS', false),
         ],
     ],
 
