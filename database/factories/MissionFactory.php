d5d66f1c9db152004f2a03447e6a4536d47e7d49
Ptitlu42 <lucas.beyer@gmx.fr>
2023-12-16 19:05:21 +0100

    Refactor missions ♻️

--- database/factories/MissionFactory.php
+++ database/factories/MissionFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Plant;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\User;
 use Illuminate\Support\Facades\Log;
@@ -24,13 +25,16 @@ class MissionFactory extends Factory
             Log::info("Creating a new mission: $description price: $price ✅");
         }
         $userIds = User::factory()->count(10)->create()->pluck('id')->toArray();
+        $plantIds = Plant::factory()->count(10)->create()->pluck('id')->toArray();
 
         return [
+            'number_of_sessions' => rand(1, 10),
+            'plants_list' => json_encode($plantIds),
             'creation_date' => $this->faker->date,
             'start_date' => $this->faker->date,
             'end_date' => $this->faker->date,
             'owner_id' => User::factory(),
-            'botanist_id' => User::factory(),
+            'gardien_id' => User::factory(),
             'candidates_list' => json_encode($userIds),
             'price' => $price,
             'description' => $description,
