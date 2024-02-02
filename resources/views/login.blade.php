9dd9a46a80a0be3d748b9d920d7ac7ed1a005b85
elliot <elliot.latter@gmail.com>
2024-02-02 17:10:44 +0100

    Update input color and add icon

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,22 +4,23 @@
     <link href="{{ asset('assets/css/login.css') }}" rel="stylesheet">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'%3E%3Cpath d='M10 0C4.48 0 0 4.48 0 10s4.48 10 10 10 10-4.48 10-10S15.52 0 10 0zM9 15v-2H8v2H5V5h2V3h2v2h2v10h-2v-2H9v2zm5-10h-2V3h2v2zm0 10h-2v2h-2v-2H7v2H5v-2H3v-2h2V7H3V5h2V3h2v2h2v10h-2v-2h-2v2H7v-2H5v2H3v-2h2v-2h2v2h2v-2h2v2h2V5h-2V3h2V1h-2V0z' fill='%2300FF00'/%3E%3C/svg%3E" type="image/svg+xml">
 
-    <title>Laravel</title>
+    <title>Arosa-je</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet"/>
     <!-- Styles -->
 </head>
-
+<body>
 <div class="container">
     <div class="left-side">
         <button class="cancel-button"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                 <path d="M7.07 7.07l9.17 9.17M7.07 16.24L16.24 7.07" fill="none" stroke="#ffffff" stroke-width="2"/>
             </svg></button>
         <div class="centered-content">
-            <form action="/login" method="post">
+            <form action="/login" method="get">
             <div class="wave-group">
                 <input required="" type="email" class="input"  pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}">
                 <span class="bar"></span>
