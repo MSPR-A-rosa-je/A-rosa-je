d629ccfc1c8d84f47eff03e8deac3bb7821db1ec
DavidC212 <davidcuomo212@gmail.com>
2024-02-02 17:08:26 +0100

    Footer, header, Home page and pictures

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,27 @@
-<footer>
+<footer class="grid grid-cols-5 gap-4 footer">
+    <div class="col-start-1 col-end-2">
+        <a href="/">
+            <img id="footer-logo" src="{{ asset('assets/pictures/site_name.png') }}"/>
+        </a>
+    </div>
+
+    <nav class="col-start-3 col-span-1">
+        <ul>
+            <li class="">
+                <a href="#">About us</a>
+            </li>
+            <li class="">
+                <a href="#">Legal notice</a>
+            </li>
+        </ul>
+    </nav>
+
+    <div class="col-end-6 col-span-1">
+        MANGE TES ICONES
+    </div>
 </footer>
 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
 </body>
 </html>
