84f8d0feb4cd3ca35cc46023a95563586b8ff099
Grizzlywawa <pierre.guerbeau@gmail.com>
2024-02-02 15:42:33 +0100

    address completed

---
+++ app/Http/Controllers/AddressController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Address;
+use Illuminate\Support\Facades\Log;
+
+class AddressController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $addresses = Address::all();
+        return view('addresses.index', compact('addresses'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('addresses.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validateData = $request->validate([
+            'address' => 'required|string|max:255',
+            'city' => 'required|string|max:255',
+            'zip_code' => 'required|integer|max:10'
+        ]);
+
+        try{
+            $address = Address::create($validateData);
+            Log::info('Address created:'.$address->id);
+            return redirect()->route('addresses.index')->with('success', 'Address'.$address->id.'created successfully.');
+        } catch (\Throwable $e){
+            return "<div>test</div>";
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Address $address)
+    {
+        return view('addresses.show', compact('address'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Address $address)
+    {
+        return view('addresses.edit', compact('address'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Address $address)
+    {
+        $validateData = $request->validate([
+            'address' => 'required|string|max:255',
+            'city' => 'required|string|max:255',
+            'zip_code' => 'required|integer|max:10'
+        ]);
+
+        $address->update($validateData);
+
+        return redirect()->route('addresses.index')->with('success', 'Adress updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Address$address)
+    {
+        try{
+            $address->delete();
+            Log::info('Address deleted:'.$adress->id);
+            return redirect()->route('addresses.index')->with(
+                'success', 'Adress'.$address->id.'deleted successfully ✅');
+        }catch(\Exception $e){
+            return redirect()->route('addresses.index')->with('error', 'An error occured while deleting the adress : '.$e->getMessage().'❌');
+        }
+    }
+}
---
+++ resources/views/addresses/show.blade.php
@@ -0,0 +1,38 @@
+@include('admin')
+<body>
+<div>
+    <h2 class="logo">Profil of '{{ $address->id }}'</h2>
+    <div class="adress-table">
+        <table>
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>Address</th>
+                <th>City</th>
+                <th>Zip Code</th>
+                <th>User ID</th>
+
+            </tr>
+            </thead>
+            <tbody>
+            <tr class="clickable-row">
+                <td>{{ $address->id }}</td>
+                <td>{{ $address->address }}</td>
+                <td>{{$address->city}}</td>
+                <td>{{ $address->zip_code }}</td>
+                <td>{{ $address->user_id }}</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+    <div>
+        <form action="{{ route('addresses.destroy', $address->id) }}" method="POST">
+            @csrf
+            @method('DELETE')
+            <a href="/addresses" class="button" style="margin-right: 66em; margin-left: 2em">Back</a>
+            <a class="button" href="{{route('addresses.edit', $address->id)}}">Edit</a>
+            <button class="button" type="submit" onclick="return confirm('Are you sure you want to delete this address?');">Delete</button>
+        </form>
+    </div>
+</div>
+</body>
