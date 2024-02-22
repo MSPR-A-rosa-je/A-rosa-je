a049e05efe7259481fa0c93634015c77dd6a22a9
Ptitlu42 <lucas.beyer@gmx.fr>
2024-02-22 02:29:17 +0100

    Sonar debug

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,9 +1,16 @@
 @section('content')
 <head>
+    <title>Login</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon"
-          href="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'%3E%3Cpath d='M10 0C4.48 0 0 4.48 0 10s4.48 10 10 10 10-4.48 10-10S15.52 0 10 0zM9 15v-2H8v2H5V5h2V3h2v2h2v10h-2v-2H9v2zm5-10h-2V3h2v2zm0 10h-2v2h-2v-2H7v2H5v-2H3v-2h2V7H3V5h2V3h2v2h2v10h-2v-2h-2v2H7v-2H5v2H3v-2h2v-2h2v2h2v-2h2v2h2V5h-2V3h2V1h-2V0z' fill='%2300FF00'/%3E%3C/svg%3E"
+          href="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg'
+          viewBox='0 0 20 20'%3E%3Cpath d='M10 0C4.48 0 0 4.48 0 10s4.48
+          10 10 10 10-4.48 10-10S15.52 0 10 0zM9
+           15v-2H8v2H5V5h2V3h2v2h2v10h-2v-2H9v2zm5-10h-2V3h2v2zm0
+            10h-2v2h-2v-2H7v2H5v-2H3v-2h2V7H3V5h2V3h2v2h2v10h-2v-2h
+            -2v2H7v-2H5v2H3v-2h2v-2h2v2h2v-2h2v2h2V5h-2V3h2V1h-2V0z'
+             fill='%2300FF00'/%3E%3C/svg%3E"
           type="image/svg+xml">
     <link href="{{ asset('assets/css/login.css') }}" rel="stylesheet">
 </head>
@@ -20,8 +27,8 @@
             <form action="{{route('authenticate')}}" method="post">
                 @csrf
                 <div class="wave-group">
-                    <input required="" type="email" class="input @error('email') is-invalid @enderror" id="email"
-                           name="email" value="{{old('email')}}">
+                    <input required="" type="email" class="input{{ $errors->has('email') ? ' is-invalid' : '' }}"
+                           id="email" name="email" value="{{ old('email') }}">
                     @if ($errors->has('email'))
                     <span class="text-danger">{{$errors->first('email')}}</span>
                     @endif
@@ -36,8 +43,8 @@
                 </div>
                 <br>
                 <div class="wave-group">
-                    <input type="password" required="" class="input @error('password') is-invalid @enderror"
-                           id="password" name="password" value="{{old('password')}}">
+                    <input type="password" required="" class="input{{ $errors->has('password') ? ' is-invalid' : '' }}"
+                           id="password" name="password" value="{{ old('password') }}">
                     @if ($errors->has('password'))
                     <span class="text-danger">{{$errors->first('password')}}</span>
                     @endif
@@ -62,7 +69,7 @@
     </div>
     <div class="right-side">
         <div class="image-overlay">Good to see you again</div>
-        <img src="{{ asset('assets/pictures/log_background.jpg') }}">
+        <img src="{{ asset('assets/pictures/log_background.jpg') }}" alt="background">
     </div>
 </div>
 </body>
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,11 +1,11 @@
 <footer class="flex-class-center footer">
     <div class="col-start-1 col-end-2">
         <a href="/">
-            <img id="footer-logo" src="{{ asset('assets/pictures/site_name.png') }}"/>
+            <img id="footer-logo" src="{{ asset('assets/pictures/site_name.png') }}" alt="site"/>
         </a>
     </div>
 
-    <nav class="nav-footer-spec">
+    <nav class="nav-footer-spec" aria-label="Main">
         <ul class="flex">
             <li class="">
                 <a href="#">About us</a>
@@ -16,7 +16,7 @@
         </ul>
     </nav>
 
-    <nav class="nav-soc-net">
+    <nav class="nav-soc-net" aria-label="Main">
         <ul class="flex-class-center">
             <li><a href="#"><i class="fa-brands fa-square-facebook"></i></a></li>
             <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
@@ -26,7 +26,9 @@
     </nav>
 </footer>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+crossorigin="anonymous"></script>
 
 <script src="{{ asset('assets/js/app.js') }}"></script>
 
